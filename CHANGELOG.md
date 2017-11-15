cc6a70d99017819804d54a467a84b793309849c0
Ashley Gould <agould@ucop.edu>
2017-11-15 14:18:51 -0800

    Initial Release 0.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+release-0.0.1 Wed Nov 15 14:16:31 PST 2017
+- initail release
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='aws-stackermods',
-    version='0.0.0.',
+    version='0.0.1.',
     description='Collection of stacker blueprint modules',
     long_description=long_description,
     url='https://github.com/ashleygould/aws-stackermods',
